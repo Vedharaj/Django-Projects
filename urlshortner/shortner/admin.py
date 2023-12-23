8c21210cda4cf71acfa34e6fcc39ba464ed5a955
Vedharaj <crisvedharaj@gmail.com>
2023-12-23 17:17:51 +0530

    Update error handle page

--- urlshortner/shortner/admin.py
+++ urlshortner/shortner/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Url
 
 # Register your models here.
+class UrlDisplay(admin.ModelAdmin):
+    list_display = ("link", "uuid")
+
+admin.site.register(Url, UrlDisplay)
